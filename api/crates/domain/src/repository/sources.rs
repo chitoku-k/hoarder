bd31b3b1bc548a84b35408570bcb1fe0b2572693
Chitoku <odango@chitoku.jp>
2023-07-14 03:21:28 +0900

    Ensure the `source` field in query returns one element

--- api/crates/domain/src/repository/sources.rs
+++ api/crates/domain/src/repository/sources.rs
@@ -14,8 +14,8 @@ pub trait SourcesRepository: Send + Sync + 'static {
     /// Creates a source.
     async fn create(&self, external_service_id: ExternalServiceId, external_metadata: ExternalMetadata) -> anyhow::Result<Source>;
 
-    /// Fetches the sources by their external metadata.
-    async fn fetch_by_external_metadata(&self, external_service_id: ExternalServiceId, external_metadata: ExternalMetadata) -> anyhow::Result<Vec<Source>>;
+    /// Fetches the source by its external metadata.
+    async fn fetch_by_external_metadata(&self, external_service_id: ExternalServiceId, external_metadata: ExternalMetadata) -> anyhow::Result<Source>;
 
     /// Updates the source by ID.
     async fn update_by_id(&self, id: SourceId, external_service_id: Option<ExternalServiceId>, external_metadata: Option<ExternalMetadata>) -> anyhow::Result<Source>;
