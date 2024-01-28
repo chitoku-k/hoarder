d6c1df8f2082e440e96d29f79dd6a07a30f910f4
Chitoku <odango@chitoku.jp>
2024-01-28 19:09:06 +0900

    Spawn a blocking task when processing an image

--- api/crates/media/src/parser.rs
+++ api/crates/media/src/parser.rs
@@ -13,15 +13,17 @@ pub struct FileImageParser;
 #[async_trait]
 impl MediumImageParser for FileImageParser {
     async fn get_metadata(&self, path: &str) -> anyhow::Result<MediumMetadata> {
-        task::block_in_place(move || {
+        let path = path.to_string();
+
+        task::spawn_blocking(move || {
             let file = File::open(path).context("failed to open image")?;
             let reader = Reader::new(BufReader::new(file))
                 .with_guessed_format()
                 .context("failed to detect image format")?;
 
             let format = reader.format().context("failed to get image format")?;
-            let image = reader.decode().context("failed to decode image")?;
-            Ok(MediumMetadata::new(format.to_mime_type(), image.width(), image.height()))
-        })
+            let (width, height) = reader.into_dimensions().context("failed to decode image")?;
+            Ok(MediumMetadata::new(format.to_mime_type(), width, height))
+        }).await?
     }
 }
