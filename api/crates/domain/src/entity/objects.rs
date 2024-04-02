2aa72d1398307c75d1ebe7024cdb838aef20bcce
Chitoku <odango@chitoku.jp>
2024-04-02 14:44:11 +0900

    Encode/decode object URLs in domain service

--- api/crates/domain/src/entity/objects.rs
+++ api/crates/domain/src/entity/objects.rs
@@ -1,6 +1,9 @@
 use chrono::{DateTime, Utc};
 use derive_more::{Constructor, Deref, Display, From};
+use percent_encoding::{percent_decode_str, utf8_percent_encode, AsciiSet, CONTROLS};
 use serde::{Deserialize, Serialize};
+
+use crate::error::{Error, ErrorKind, Result};
 
 #[derive(Clone, Debug, Default, Deref, Deserialize, Display, Eq, From, Hash, PartialEq, Serialize)]
 pub struct EntryUrl(String);
@@ -32,28 +35,94 @@ pub enum EntryKind {
 }
 
 impl EntryUrl {
+    const RFC3986_PATH: &'static AsciiSet = &CONTROLS
+        .add(b' ')
+        .add(b'"')
+        .add(b'#')
+        .add(b'%')
+        .add(b'<')
+        .add(b'>')
+        .add(b'?')
+        .add(b'^')
+        .add(b'`')
+        .add(b'{')
+        .add(b'}');
+
+    pub fn from_path_str(prefix: &str, path: &str) -> Self {
+        let url = format!(
+            "{}{}",
+            prefix,
+            utf8_percent_encode(path, Self::RFC3986_PATH),
+        );
+
+        Self::from(url)
+    }
+
+    pub fn to_path_string(&self, prefix: &str) -> Result<String> {
+        let path = self
+            .strip_prefix(prefix)
+            .ok_or_else(|| ErrorKind::ObjectUrlUnsupported { url: self.to_string() })?;
+
+        let path = percent_decode_str(path)
+            .decode_utf8()
+            .map_err(|e| Error::new(ErrorKind::ObjectPathInvalid, e))?
+            .to_string();
+
+        Ok(path)
+    }
+
     pub fn into_inner(self) -> String {
         self.0
     }
 }
 
 impl EntryUrlPath {
-    pub fn into_inner(self) -> String {
-        self.0
-    }
-
     pub fn to_url(&self, scheme: &str) -> EntryUrl {
         let path = &self.0;
         let url = format!("{scheme}://{path}");
         EntryUrl::from(url)
     }
+
+    pub fn into_inner(self) -> String {
+        self.0
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use pretty_assertions::assert_eq;
+    use pretty_assertions::{assert_eq, assert_matches};
 
-    use crate::entity::objects::{EntryUrl, EntryUrlPath};
+    use crate::{entity::objects::{EntryUrl, EntryUrlPath}, error::ErrorKind};
+
+    #[test]
+    fn entry_url_from_path_str() {
+        let actual = EntryUrl::from_path_str("file://", "/ゆるゆり/77777777-7777-7777-7777-777777777777.png");
+        assert_eq!(actual, EntryUrl::from("file:///%E3%82%86%E3%82%8B%E3%82%86%E3%82%8A/77777777-7777-7777-7777-777777777777.png".to_string()));
+    }
+
+    #[test]
+    fn entry_url_to_path_string_prefix_mismatch() {
+        let url = EntryUrl::from("file:///77777777-7777-7777-7777-777777777777.png".to_string());
+
+        let actual = url.to_path_string("s3://").unwrap_err();
+        assert_matches!(actual.kind(), ErrorKind::ObjectUrlUnsupported { url } if url == "file:///77777777-7777-7777-7777-777777777777.png");
+    }
+
+    #[test]
+    fn entry_url_to_path_string_utf8_valid() {
+        let url = EntryUrl::from("file:///%E3%82%86%E3%82%8B%E3%82%86%E3%82%8A/77777777-7777-7777-7777-777777777777.png".to_string());
+
+        let actual = url.to_path_string("file://").unwrap();
+        assert_eq!(actual, "/ゆるゆり/77777777-7777-7777-7777-777777777777.png".to_string());
+    }
+
+    #[test]
+    fn entry_url_to_path_string_utf8_invalid() {
+        let url = EntryUrl::from("file:///%80.png".to_string());
+
+        let actual = url.to_path_string("file://").unwrap_err();
+        assert_matches!(actual.kind(), ErrorKind::ObjectPathInvalid);
+    }
 
     #[test]
     fn entry_url_into_inner() {
@@ -64,18 +133,18 @@ mod tests {
     }
 
     #[test]
-    fn entry_url_path_into_inner() {
-        let url = EntryUrlPath::from("/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string());
-
-        let actual = url.into_inner();
-        assert_eq!(actual, "/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string());
-    }
-
-    #[test]
     fn entry_url_path_to_url() {
         let path = EntryUrlPath::from("/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string());
 
         let actual = path.to_url("file");
         assert_eq!(actual, EntryUrl::from("file:///aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string()));
+    }
+
+    #[test]
+    fn entry_url_path_into_inner() {
+        let url = EntryUrlPath::from("/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string());
+
+        let actual = url.into_inner();
+        assert_eq!(actual, "/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa.jpg".to_string());
     }
 }
--- api/crates/storage/src/lib.rs
+++ api/crates/storage/src/lib.rs
@@ -1,8 +1,4 @@
-use domain::{
-    entity::objects::{Entry, EntryUrl},
-    error::{Error, ErrorKind, Result},
-};
-use percent_encoding::{percent_decode_str, utf8_percent_encode, AsciiSet, CONTROLS};
+use domain::{entity::objects::{Entry, EntryUrl}, error::Error};
 
 pub mod filesystem;
 
@@ -12,42 +8,6 @@ pub(crate) trait StorageEntry {
 
 pub(crate) trait StorageEntryUrl: TryFrom<EntryUrl, Error = Error> {
     const URL_PREFIX: &'static str;
-
-    const RFC3986_PATH: &'static AsciiSet = &CONTROLS
-        .add(b' ')
-        .add(b'"')
-        .add(b'#')
-        .add(b'%')
-        .add(b'<')
-        .add(b'>')
-        .add(b'?')
-        .add(b'^')
-        .add(b'`')
-        .add(b'{')
-        .add(b'}');
-
-    fn from_path_str(path: &str) -> Result<Self> {
-        let url = format!(
-            "{}{}",
-            Self::URL_PREFIX,
-            utf8_percent_encode(path, Self::RFC3986_PATH),
-        );
-
-        Self::try_from(EntryUrl::from(url))
-    }
-
-    fn to_path_string(url: &str) -> Result<String> {
-        let path = url
-            .strip_prefix(Self::URL_PREFIX)
-            .ok_or_else(|| ErrorKind::ObjectUrlUnsupported { url: url.to_string() })?;
-
-        let path = percent_decode_str(path)
-            .decode_utf8()
-            .map_err(|e| Error::new(ErrorKind::ObjectPathInvalid, e))?
-            .to_string();
-
-        Ok(path)
-    }
 
     fn into_url(self) -> EntryUrl;
 }
