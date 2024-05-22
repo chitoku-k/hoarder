a0481072c6510ce3f030c361b7af8c9628ebe8f7
Chitoku <odango@chitoku.jp>
2024-05-23 03:13:03 +0900

    Sort media tags in hierarchical order

--- api/crates/postgres/src/expr/array.rs
+++ api/crates/postgres/src/expr/array.rs
@@ -3,6 +3,13 @@ use sea_query::{Expr, SimpleExpr};
 pub(crate) struct ArrayExpr;
 
 impl ArrayExpr {
+    pub fn agg<T>(arg: T) -> SimpleExpr
+    where
+        T: Into<SimpleExpr>,
+    {
+        Expr::cust_with_expr("array_agg($1)", arg)
+    }
+
     pub fn unnest<T>(arg: T) -> SimpleExpr
     where
         T: Into<SimpleExpr>,
