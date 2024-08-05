c84ccc01f9c9be29793f022fbf9cb0666123b882
Chitoku <odango@chitoku.jp>
2024-08-06 03:12:04 +0900

    Add a search box in `TagTypeListView`

--- ui/src/components/TagTypeListColumnBodyList/index.tsx
+++ ui/src/components/TagTypeListColumnBodyList/index.tsx
@@ -1,14 +1,19 @@
 'use client'
 
-import type { FunctionComponent, MouseEvent } from 'react'
+import type { ComponentPropsWithoutRef, FunctionComponent, MouseEvent, SyntheticEvent } from 'react'
 import { useCallback, useState } from 'react'
-import Button from '@mui/material/Button'
+import clsx from 'clsx'
+import type { AutocompleteInputChangeReason } from '@mui/material/Autocomplete'
 import IconButton from '@mui/material/IconButton'
 import List from '@mui/material/List'
 import Stack from '@mui/material/Stack'
+import AddIcon from '@mui/icons-material/Add'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import LabelIcon from '@mui/icons-material/Label'
+import SearchIcon from '@mui/icons-material/Search'
 
+import AutocompleteTagType from '@/components/AutocompleteTagType'
 import TagTypeListColumnBodyListItem from '@/components/TagTypeListColumnBodyListItem'
 import { useAllTagTypes } from '@/hooks'
 import type { TagType } from '@/types'
@@ -19,14 +24,18 @@ const TagTypeListColumnBodyList: FunctionComponent<TagTypeListColumnBodyListProp
   creating,
   editing,
   active,
+  hit,
+  hitInput,
   readonly,
   dense,
   disabled: disabledTagType,
   onSelect: onSelectTagType,
+  onHit: onHitTagType,
   show: showTagType,
   create: createTagType,
   edit: editTagType,
   delete: deleteTagType,
+  setColumn,
 }) => {
   const allTagTypes = useAllTagTypes()
 
@@ -51,8 +60,26 @@ const TagTypeListColumnBodyList: FunctionComponent<TagTypeListColumnBodyListProp
 
   const handleClickTagType = (tagType: TagType) => {
     onSelectTagType?.(tagType)
+    onHitTagType?.(null)
     showTagType(tagType)
   }
+
+  const handleHitTagType = useCallback((tagType: TagType | null) => {
+    onHitTagType?.(tagType)
+  }, [ onHitTagType ])
+
+  const handleInputHitTagType = useCallback((_e: SyntheticEvent, value: string, reason: AutocompleteInputChangeReason) => {
+    if (!value && reason === 'input') {
+      onHitTagType?.(null)
+    }
+    setColumn({
+      creating,
+      editing,
+      active,
+      hit,
+      hitInput: value,
+    })
+  }, [ onHitTagType, setColumn, creating, editing, active, hit ])
 
   const handleClickCreateTagType = useCallback(() => {
     createTagType()
@@ -76,14 +103,50 @@ const TagTypeListColumnBodyList: FunctionComponent<TagTypeListColumnBodyListProp
     e.stopPropagation()
   }, [])
 
+  const renderTagTypeOption = useCallback(({ key, ...props }: ComponentPropsWithoutRef<'li'>, option: TagType) => (
+    <li key={key} {...props}>
+      <Stack direction="row" spacing={0.5} alignItems="start">
+        <LabelIcon className={styles.tagTypeSearchIcon} fontSize="small" />
+        <span className={styles.tagTypeSearchText}>{option.name}</span>
+      </Stack>
+    </li>
+  ), [])
+
   return (
     <Stack className={styles.container}>
-      <Stack className={styles.buttons}>
-        {!readonly ? (
-          <Button variant="outlined" onClick={handleClickCreateTagType}>
-            新規作成
-          </Button>
-        ) : null}
+      <Stack className={clsx(styles.title, !readonly && styles.buttons)}>
+        <Stack direction="row" spacing={1} alignItems="center" justifyContent="space-between">
+          <AutocompleteTagType
+            className={styles.tagTypeSearch}
+            size="small"
+            variant="standard"
+            fullWidth
+            autoHighlight
+            blurOnSelect
+            clearOnBlur={false}
+            clearOnEscape
+            includeInputInList
+            forcePopupIcon={false}
+            placeholder="検索"
+            renderOption={renderTagTypeOption}
+            value={hit}
+            inputValue={hitInput}
+            icon={({ ...props }) => <SearchIcon fontSize="small" {...props} />}
+            onChange={handleHitTagType}
+            onInputChange={handleInputHitTagType}
+            slotProps={{
+              popper: {
+                className: styles.tagTypeSearchPopper,
+                placement: 'bottom-start',
+              },
+            }}
+          />
+          {!readonly ? (
+            <IconButton size="small" onClick={handleClickCreateTagType}>
+              <AddIcon />
+            </IconButton>
+          ) : null}
+        </Stack>
       </Stack>
       <List ref={ref} dense={dense} className={styles.tagTypes}>
         {allTagTypes.map(tagType => (
@@ -135,17 +198,21 @@ export interface TagTypeColumn {
   creating: boolean
   editing: TagType | null
   active: TagType | null
+  hit: TagType | null
+  hitInput: string
 }
 
 export interface TagTypeListColumnBodyListProps extends TagTypeColumn {
   readonly: boolean
   dense: boolean
   disabled?: (tagType: TagType) => boolean
+  onHit?: (tagType: TagType | null) => void
   onSelect?: (tagType: TagType) => void
   create: () => void
   show: (tagType: TagType) => void
   edit: (tagType: TagType) => void
   delete: (tagType: TagType) => void
+  setColumn: (column: TagTypeColumn) => void
 }
 
 export default TagTypeListColumnBodyList
