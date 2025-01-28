d4f2d71a2c647a085cc7f77d5783da4261b48c25
Chitoku <odango@chitoku.jp>
2025-01-28 17:43:46 +0900

    Add tests for mutation in graphql

---
+++ api/crates/graphql/tests/mutation-attach_tag.rs
@@ -0,0 +1,111 @@
+use std::{collections::BTreeSet, sync::Arc};
+
+use async_graphql::{value, EmptySubscription, Schema};
+use chrono::{TimeZone, Utc};
+use domain::entity::tags::{AliasSet, Tag, TagDepth, TagId};
+use futures::future::ok;
+use graphql::{mutation::Mutation, query::Query};
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+use uuid::uuid;
+
+mod mocks;
+use mocks::{
+    domain::service::{
+        external_services::MockExternalServicesServiceInterface,
+        media::MockMediaServiceInterface,
+        tags::MockTagsServiceInterface,
+    },
+    normalizer::MockNormalizerInterface,
+};
+
+#[tokio::test]
+async fn succeeds() {
+    let external_services_service = MockExternalServicesServiceInterface::new();
+    let media_service = MockMediaServiceInterface::new();
+    let normalizer = MockNormalizerInterface::new();
+
+    let mut tags_service = MockTagsServiceInterface::new();
+    tags_service
+        .expect_attach_tag_by_id()
+        .times(1)
+        .withf(|id, parent_id, depth| (id, parent_id, depth) == (
+            &TagId::from(uuid!("33333333-3333-3333-3333-333333333333")),
+            &TagId::from(uuid!("22222222-2222-2222-2222-222222222222")),
+            &TagDepth::new(1, 0),
+        ))
+        .returning(|_, _, _| {
+            Box::pin(ok(Tag {
+                id: TagId::from(uuid!("33333333-3333-3333-3333-333333333333")),
+                name: "赤座あかり".to_string(),
+                kana: "あかざあかり".to_string(),
+                aliases: AliasSet::new(BTreeSet::from(["アッカリーン".to_string()])),
+                parent: Some(Box::new(Tag {
+                    id: TagId::from(uuid!("22222222-2222-2222-2222-222222222222")),
+                    name: "ゆるゆり".to_string(),
+                    kana: "ゆるゆり".to_string(),
+                    aliases: Default::default(),
+                    parent: None,
+                    children: Vec::new(),
+                    created_at: Utc.with_ymd_and_hms(2022, 6, 1, 0, 0, 0).unwrap(),
+                    updated_at: Utc.with_ymd_and_hms(2022, 6, 1, 0, 1, 0).unwrap(),
+                })),
+                children: Vec::new(),
+                created_at: Utc.with_ymd_and_hms(2022, 6, 1, 0, 0, 0).unwrap(),
+                updated_at: Utc.with_ymd_and_hms(2022, 6, 1, 0, 1, 0).unwrap(),
+            }))
+        });
+
+    let query = Query::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let mutation = Mutation::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let schema = Schema::build(query, mutation, EmptySubscription)
+        .data(external_services_service)
+        .data(media_service)
+        .data(tags_service)
+        .data(Arc::new(normalizer))
+        .finish();
+
+    let req = indoc! {r#"
+        mutation {
+            attachTag(
+                id: "33333333-3333-3333-3333-333333333333",
+                parentId: "22222222-2222-2222-2222-222222222222",
+            ) {
+                id
+                name
+                kana
+                aliases
+                parent {
+                    id
+                    name
+                    kana
+                    aliases
+                    createdAt
+                    updatedAt
+                }
+                createdAt
+                updatedAt
+            }
+        }
+    "#};
+    let actual = schema.execute(req).await.into_result().unwrap();
+
+    assert_eq!(actual.data, value!({
+        "attachTag": {
+            "id": "33333333-3333-3333-3333-333333333333",
+            "name": "赤座あかり",
+            "kana": "あかざあかり",
+            "aliases": ["アッカリーン"],
+            "parent": {
+                "id": "22222222-2222-2222-2222-222222222222",
+                "name": "ゆるゆり",
+                "kana": "ゆるゆり",
+                "aliases": [],
+                "createdAt": "2022-06-01T00:00:00+00:00",
+                "updatedAt": "2022-06-01T00:01:00+00:00",
+            },
+            "createdAt": "2022-06-01T00:00:00+00:00",
+            "updatedAt": "2022-06-01T00:01:00+00:00",
+        },
+    }));
+}
---
+++ api/crates/graphql/tests/mutation-delete_source.rs
@@ -0,0 +1,57 @@
+use std::sync::Arc;
+
+use async_graphql::{Schema, EmptySubscription, value};
+use domain::{entity::sources::SourceId, repository::DeleteResult};
+use futures::future::ok;
+use graphql::{mutation::Mutation, query::Query};
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+use uuid::uuid;
+
+mod mocks;
+use mocks::{
+    domain::service::{
+        external_services::MockExternalServicesServiceInterface,
+        media::MockMediaServiceInterface,
+        tags::MockTagsServiceInterface,
+    },
+    normalizer::MockNormalizerInterface,
+};
+
+#[tokio::test]
+async fn succeeds() {
+    let external_services_service = MockExternalServicesServiceInterface::new();
+    let tags_service = MockTagsServiceInterface::new();
+    let normalizer = MockNormalizerInterface::new();
+
+    let mut media_service = MockMediaServiceInterface::new();
+    media_service
+        .expect_delete_source_by_id()
+        .times(1)
+        .withf(|id| id == &SourceId::from(uuid!("11111111-1111-1111-1111-111111111111")))
+        .returning(|_| Box::pin(ok(DeleteResult::Deleted(1))));
+
+    let query = Query::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let mutation = Mutation::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let schema = Schema::build(query, mutation, EmptySubscription)
+        .data(external_services_service)
+        .data(media_service)
+        .data(tags_service)
+        .data(Arc::new(normalizer))
+        .finish();
+
+    let req = indoc! {r#"
+        mutation {
+            deleteSource(id: "11111111-1111-1111-1111-111111111111") {
+                deleted
+            }
+        }
+    "#};
+    let actual = schema.execute(req).await.into_result().unwrap();
+
+    assert_eq!(actual.data, value!({
+        "deleteSource": {
+            "deleted": true,
+        },
+    }));
+}
