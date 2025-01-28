1e6003e4eb6dca7b5c40beb2100e4defa0d4a8e0
Chitoku <odango@chitoku.jp>
2025-01-28 17:43:46 +0900

    Add tests for mutation in graphql

---
+++ api/crates/graphql/tests/mutation-create_external_service.rs
@@ -0,0 +1,100 @@
+use std::{borrow::Cow, sync::Arc};
+
+use async_graphql::{Schema, EmptySubscription, value};
+use domain::entity::external_services::{ExternalService, ExternalServiceId};
+use futures::future::ok;
+use graphql::{mutation::Mutation, query::Query};
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+use uuid::uuid;
+
+mod mocks;
+use mocks::{
+    domain::service::{
+        external_services::MockExternalServicesServiceInterface,
+        media::MockMediaServiceInterface,
+        tags::MockTagsServiceInterface,
+    },
+    normalizer::MockNormalizerInterface,
+};
+
+#[tokio::test]
+async fn succeeds() {
+    let mut external_services_service = MockExternalServicesServiceInterface::new();
+    external_services_service
+        .expect_create_external_service()
+        .times(1)
+        .withf(|slug, kind, name, base_url, url_pattern| (slug, kind, name, base_url, url_pattern) == (
+            "x",
+            "x",
+            "X",
+            &Some("https://x.com"),
+            &Some(r"^https?://(?:twitter\.com|x\.com)/(?<creatorId>[^/]+)/status/(?<id>\d+)(?:[/?#].*)?$"),
+         ))
+        .returning(|_, _, _, _, _| {
+            Box::pin(ok(ExternalService {
+                id: ExternalServiceId::from(uuid!("33333333-3333-3333-3333-333333333333")),
+                slug: "x".to_string(),
+                kind: "x".to_string(),
+                name: "X".to_string(),
+                base_url: Some("https://x.com".to_string()),
+                url_pattern: Some(r"^https?://(?:twitter\.com|x\.com)/(?<creatorId>[^/]+)/status/(?<id>\d+)(?:[/?#].*)?$".to_string()),
+            }))
+        });
+
+    let media_service = MockMediaServiceInterface::new();
+    let tags_service = MockTagsServiceInterface::new();
+
+    let mut normalizer = MockNormalizerInterface::new();
+    normalizer
+        .expect_normalize_str()
+        .times(2)
+        .withf(|text| text == "x")
+        .returning(|_| Cow::from("x"));
+
+    normalizer
+        .expect_normalize_str()
+        .times(1)
+        .withf(|text| text == "X")
+        .returning(|_| Cow::from("X"));
+
+    let query = Query::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let mutation = Mutation::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let schema = Schema::build(query, mutation, EmptySubscription)
+        .data(external_services_service)
+        .data(media_service)
+        .data(tags_service)
+        .data(Arc::new(normalizer))
+        .finish();
+
+    let req = indoc! {r#"
+        mutation {
+            createExternalService(
+                slug: "x",
+                kind: "x",
+                name: "X",
+                baseUrl: "https://x.com",
+                urlPattern: "^https?://(?:twitter\\.com|x\\.com)/(?<creatorId>[^/]+)/status/(?<id>\\d+)(?:[/?#].*)?$",
+            ) {
+                id
+                slug
+                kind
+                name
+                baseUrl
+                urlPattern
+            }
+        }
+    "#};
+    let actual = schema.execute(req).await.into_result().unwrap();
+
+    assert_eq!(actual.data, value!({
+        "createExternalService": {
+            "id": "33333333-3333-3333-3333-333333333333",
+            "slug": "x",
+            "kind": "x",
+            "name": "X",
+            "baseUrl": "https://x.com",
+            "urlPattern": r"^https?://(?:twitter\.com|x\.com)/(?<creatorId>[^/]+)/status/(?<id>\d+)(?:[/?#].*)?$",
+        },
+    }));
+}
---
+++ api/crates/graphql/tests/mutation-delete_replica.rs
@@ -0,0 +1,63 @@
+use std::sync::Arc;
+
+use async_graphql::{Schema, EmptySubscription, value};
+use domain::{entity::replicas::ReplicaId, repository::DeleteResult};
+use futures::future::ok;
+use graphql::{mutation::Mutation, query::Query};
+use indoc::indoc;
+use pretty_assertions::assert_eq;
+use uuid::uuid;
+
+mod mocks;
+use mocks::{
+    domain::service::{
+        external_services::MockExternalServicesServiceInterface,
+        media::MockMediaServiceInterface,
+        tags::MockTagsServiceInterface,
+    },
+    normalizer::MockNormalizerInterface,
+};
+
+#[tokio::test]
+async fn succeeds() {
+    let external_services_service = MockExternalServicesServiceInterface::new();
+    let tags_service = MockTagsServiceInterface::new();
+    let normalizer = MockNormalizerInterface::new();
+
+    let mut media_service = MockMediaServiceInterface::new();
+    media_service
+        .expect_delete_replica_by_id()
+        .times(1)
+        .withf(|id, delete_object| (id, delete_object) == (
+            &ReplicaId::from(uuid!("66666666-6666-6666-6666-666666666666")),
+            &true,
+        ))
+        .returning(|_, _| Box::pin(ok(DeleteResult::Deleted(1))));
+
+    let query = Query::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let mutation = Mutation::<MockExternalServicesServiceInterface, MockMediaServiceInterface, MockTagsServiceInterface, MockNormalizerInterface>::new();
+    let schema = Schema::build(query, mutation, EmptySubscription)
+        .data(external_services_service)
+        .data(media_service)
+        .data(tags_service)
+        .data(Arc::new(normalizer))
+        .finish();
+
+    let req = indoc! {r#"
+        mutation {
+            deleteReplica(
+                id: "66666666-6666-6666-6666-666666666666",
+                deleteObject: true,
+            ) {
+                deleted
+            }
+        }
+    "#};
+    let actual = schema.execute(req).await.into_result().unwrap();
+
+    assert_eq!(actual.data, value!({
+        "deleteReplica": {
+            "deleted": true,
+        },
+    }));
+}
