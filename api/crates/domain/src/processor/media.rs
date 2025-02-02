6636e3916bfa62199fe2ee047e7a040e859bc1c3
Chitoku <odango@chitoku.jp>
2025-02-02 21:53:00 +0900

    Use `mockall::concretize` for mocking `MediumImageProcessor::generate_thumbnail()`

--- api/crates/domain/src/processor/media.rs
+++ api/crates/domain/src/processor/media.rs
@@ -1,13 +1,12 @@
-use std::io::{BufRead, Seek};
-
 use crate::{
     entity::replicas::{OriginalImage, ThumbnailImage},
     error::Result,
+    io::SeekableBufRead,
 };
 
 pub trait MediumImageProcessor: Send + Sync + 'static {
     /// Generates a thumbnail for image on the given path.
     fn generate_thumbnail<R>(&self, read: R) -> Result<(OriginalImage, ThumbnailImage)>
     where
-        for<'a> R: BufRead + Seek + 'a;
+        R: SeekableBufRead;
 }
