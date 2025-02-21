d0f130e9f01abbcaacd93898e18b0f77431cf81c
Chitoku <odango@chitoku.jp>
2025-02-21 11:37:29 +0900

    Fix incorrect lifetimes in mock `impl`s

--- api/crates/graphql/src/tests/mocks/domain/service/external_services.rs
+++ api/crates/graphql/src/tests/mocks/domain/service/external_services.rs
@@ -12,7 +12,7 @@ mockall::mock! {
     pub(crate) ExternalServicesServiceInterface {}
 
     impl ExternalServicesServiceInterface for ExternalServicesServiceInterface {
-        fn create_external_service<'a>(&self, slug: &str, kind: ExternalServiceKind, name: &str, base_url: Option<&'a str>, url_pattern: Option<&'a str>) -> impl Future<Output = Result<ExternalService>> + Send;
+        fn create_external_service<'a, 'b>(&self, slug: &str, kind: ExternalServiceKind, name: &str, base_url: Option<&'a str>, url_pattern: Option<&'b str>) -> impl Future<Output = Result<ExternalService>> + Send;
 
         fn get_external_services(&self) -> impl Future<Output = Result<Vec<ExternalService>>> + Send;
 
@@ -23,7 +23,7 @@ mockall::mock! {
 
         fn get_external_services_by_url(&self, url: &str) -> impl Future<Output = Result<Vec<(ExternalService, ExternalMetadata)>>> + Send;
 
-        fn update_external_service_by_id<'a>(&self, id: ExternalServiceId, slug: Option<&'a str>, name: Option<&'a str>, base_url: Option<Option<&'a str>>, url_pattern: Option<Option<&'a str>>) -> impl Future<Output = Result<ExternalService>> + Send;
+        fn update_external_service_by_id<'a, 'b, 'c, 'd>(&self, id: ExternalServiceId, slug: Option<&'a str>, name: Option<&'b str>, base_url: Option<Option<&'c str>>, url_pattern: Option<Option<&'d str>>) -> impl Future<Output = Result<ExternalService>> + Send;
 
         fn delete_external_service_by_id(&self, id: ExternalServiceId) -> impl Future<Output = Result<DeleteResult>> + Send;
     }
