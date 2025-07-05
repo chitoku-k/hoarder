56225df0c8db90acdde30bf1e1f36242a257b895
Chitoku <odango@chitoku.jp>
2025-07-05 22:01:36 +0900

    Fix a failing test for GraphQL SDL

--- api/crates/graphql/src/tests.rs
+++ api/crates/graphql/src/tests.rs
@@ -43,7 +43,7 @@ fn graphql_definitions() {
     let actual = service.definitions();
     assert!(actual.contains(indoc! {"
         type Query {
-        	value: String!
+            value: String!
         }
     "}));
 }
