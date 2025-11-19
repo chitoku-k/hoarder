1e18ca306d0e49cb5063c01dc4ca66a17632bde1
Chitoku <odango@chitoku.jp>
2025-11-19 21:05:14 +0900

    Disable `IPV6_V6ONLY` even when binding on Windows

--- api/crates/application/src/tests/server.rs
+++ api/crates/application/src/tests/server.rs
@@ -1,4 +1,4 @@
-use std::net::{Ipv6Addr, SocketAddr};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
 
 use hyper::{StatusCode, Uri};
 use pretty_assertions::{assert_eq, assert_ne};
@@ -20,6 +20,41 @@ use super::{mocks::{self, application::service::{
 mod graphql;
 mod objects;
 mod thumbnails;
+
+#[tokio::test]
+async fn start_http_succeeds_with_ipv4() {
+    let mut mock_graphql_service = MockGraphQLServiceInterface::new();
+    mock_graphql_service
+        .expect_endpoints()
+        .times(1)
+        .returning(|| GraphQLEndpoints::new("/graphql", "/graphql/subscriptions"));
+
+    let mock_objects_service = MockObjectsServiceInterface::new();
+    let mock_thumbnails_service = MockThumbnailsServiceInterface::new();
+
+    let server = Engine::new(mock_graphql_service, mock_objects_service, mock_thumbnails_service).start(0, None).unwrap();
+    let actual = server.handle.listening().await.unwrap();
+    assert_eq!(actual.ip(), Ipv6Addr::UNSPECIFIED);
+    assert_ne!(actual.port(), 0);
+
+    let uri = Uri::builder()
+        .scheme("http")
+        .authority(SocketAddr::from((Ipv4Addr::LOCALHOST, actual.port())).to_string())
+        .path_and_query("/healthz")
+        .build()
+        .unwrap();
+
+    let client = ClientBuilder::new().build().unwrap();
+    let actual = client.get(uri.to_string()).send().await.unwrap();
+    assert_eq!(actual.status(), StatusCode::OK);
+
+    let actual = actual.bytes().await.unwrap();
+    let actual = String::from_utf8(actual.to_vec()).unwrap();
+    assert_eq!(actual, "OK");
+
+    server.handle.shutdown();
+    server.shutdown.await.unwrap().unwrap();
+}
 
 #[tokio::test]
 async fn start_http_succeeds_with_ipv6() {
@@ -45,6 +80,51 @@ async fn start_http_succeeds_with_ipv6() {
         .unwrap();
 
     let client = ClientBuilder::new().build().unwrap();
+    let actual = client.get(uri.to_string()).send().await.unwrap();
+    assert_eq!(actual.status(), StatusCode::OK);
+
+    let actual = actual.bytes().await.unwrap();
+    let actual = String::from_utf8(actual.to_vec()).unwrap();
+    assert_eq!(actual, "OK");
+
+    server.handle.shutdown();
+    server.shutdown.await.unwrap().unwrap();
+}
+
+#[cfg(feature = "tls")]
+#[cfg_attr(not(unix), ignore = "native-tls does not yet support TLS 1.3 (sfackler/rust-native-tls#140)")]
+#[tokio::test]
+async fn start_https_succeeds_with_ipv4() {
+    let mut mock_graphql_service = MockGraphQLServiceInterface::new();
+    mock_graphql_service
+        .expect_endpoints()
+        .times(1)
+        .returning(|| GraphQLEndpoints::new("/graphql", "/graphql/subscriptions"));
+
+    let mock_objects_service = MockObjectsServiceInterface::new();
+    let mock_thumbnails_service = MockThumbnailsServiceInterface::new();
+
+    let (ca, cert, key) = generate_certificate().await;
+    let cert_path = cert.path().to_str().unwrap();
+    let key_path = key.path().to_str().unwrap();
+
+    let server = Engine::new(mock_graphql_service, mock_objects_service, mock_thumbnails_service).start(0, Some((cert_path.to_string(), key_path.to_string()))).unwrap();
+    let actual = server.handle.listening().await.unwrap();
+    assert_eq!(actual.ip(), Ipv6Addr::UNSPECIFIED);
+    assert_ne!(actual.port(), 0);
+
+    let uri = Uri::builder()
+        .scheme("https")
+        .authority(SocketAddr::from((Ipv4Addr::LOCALHOST, actual.port())).to_string())
+        .path_and_query("/healthz")
+        .build()
+        .unwrap();
+
+    let client = ClientBuilder::new()
+        .tls_certs_only(Certificate::from_pem_bundle(&ca).unwrap())
+        .build()
+        .unwrap();
+
     let actual = client.get(uri.to_string()).send().await.unwrap();
     assert_eq!(actual.status(), StatusCode::OK);
 
