8cd961692de6cb81dc2baaeaaad06b349b284f79
Chitoku <odango@chitoku.jp>
2025-12-29 12:46:30 +0900

    Show processing or error replicas when viewing or editing media in UI

--- ui/src/components/MediumItemImageItem/index.tsx
+++ ui/src/components/MediumItemImageItem/index.tsx
@@ -9,6 +9,8 @@ import PhotoIcon from '@mui/icons-material/Photo'
 import Image from '@/components/Image'
 import ImageBodyBlob from '@/components/ImageBodyBlob'
 import ImageBodyNext from '@/components/ImageBodyNext'
+import ImageError from '@/components/ImageError'
+import ImageLoading from '@/components/ImageLoading'
 import type { ReplicaCreate } from '@/components/MediumItemImageEdit'
 import { isReplica } from '@/components/MediumItemImageEdit'
 import type { Replica } from '@/types'
@@ -25,42 +27,49 @@ const MediumItemImageItem: FunctionComponent<MediumItemImageItemProps> = ({
     : undefined
 
   const style = { aspectRatio } satisfies CSSProperties
+  const phase = isReplica(replica) ? replica.status.phase : 'READY'
 
-  return typeof replica.width === 'number' && typeof replica.height === 'number' ? (
+  return (
     <Stack className={clsx(styles.wrapper, className)} alignItems="stretch" justifyContent="stretch" style={style}>
-      <Image
-        className={clsx(styles.item)}
-        width={replica.width}
-        height={replica.height}
-        style={style}
-      >
-        {isReplica(replica) ? replica.url ? (
-          <ImageBodyNext
-            className={styles.image}
-            src={replica.url}
-            width={replica.width}
-            height={replica.height}
-            preload
-            unoptimized
-            alt=""
-          />
-        ) : (
-          <Stack className={styles.noimage} alignItems="center" justifyContent="center">
-            <PhotoIcon className={styles.noimageIcon} />
-          </Stack>
-        ) : (
-          <ImageBodyBlob
-            className={styles.image}
-            src={replica.blob}
-            width={replica.width}
-            height={replica.height}
-            alt=""
-          />
-        )}
-      </Image>
+      {phase === 'READY' && typeof replica.width === 'number' && typeof replica.height === 'number' ? (
+        <Image
+          className={styles.item}
+          width={replica.width}
+          height={replica.height}
+          style={style}
+        >
+          {isReplica(replica) ? replica.url ? (
+            <ImageBodyNext
+              className={styles.image}
+              src={replica.url}
+              width={replica.width}
+              height={replica.height}
+              preload
+              unoptimized
+              alt=""
+            />
+          ) : (
+            <Stack className={styles.noimage} alignItems="center" justifyContent="center">
+              <PhotoIcon className={styles.noimageIcon} />
+            </Stack>
+          ) : (
+            <ImageBodyBlob
+              className={styles.image}
+              src={replica.blob}
+              width={replica.width}
+              height={replica.height}
+              alt=""
+            />
+          )}
+        </Image>
+      ) : phase === 'PROCESSING' ? (
+        <ImageLoading className={styles.loading} />
+      ) : phase === 'ERROR' ? (
+        <ImageError className={styles.error} />
+      ) : null}
       {children}
     </Stack>
-  ) : null
+  )
 }
 
 export interface MediumItemImageItemProps {
