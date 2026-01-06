5ac26d681dc7dab42998fb43a0e92b83193d49e3
Chitoku <odango@chitoku.jp>
2026-01-06 19:40:31 +0900

    Fix test cases in `FilesystemObjectsRepository`

--- api/crates/storage/src/tests/filesystem.rs
+++ api/crates/storage/src/tests/filesystem.rs
@@ -466,7 +466,7 @@ async fn entry_fails_with_parent_directory() {
     File::create_new(root_dir.path().join("ゆるゆり").join("77777777-7777-7777-7777-777777777777.png")).await.unwrap();
 
     let repository = FilesystemObjectsRepository::new(collator, root_dir.path()).await.unwrap();
-    let actual = repository.get(EntryUrl::from_path_str("file://", "/../ゆるゆり/77777777-7777-7777-7777-777777777777.png")).await.unwrap_err();
+    let actual = repository.entry(EntryUrl::from_path_str("file://", "/../ゆるゆり/77777777-7777-7777-7777-777777777777.png")).await.unwrap_err();
 
     let expected_url = "file:///../%E3%82%86%E3%82%8B%E3%82%86%E3%82%8A/77777777-7777-7777-7777-777777777777.png";
     assert_matches!(actual.kind(), ErrorKind::ObjectUrlInvalid { url } if url == expected_url);
@@ -478,7 +478,7 @@ async fn entry_fails_with_not_found() {
     let root_dir = tempdir().unwrap();
 
     let repository = FilesystemObjectsRepository::new(collator, root_dir.path()).await.unwrap();
-    let actual = repository.get(EntryUrl::from_path_str("file://", "/ゆるゆり/77777777-7777-7777-7777-777777777777.png")).await.unwrap_err();
+    let actual = repository.entry(EntryUrl::from_path_str("file://", "/ゆるゆり/77777777-7777-7777-7777-777777777777.png")).await.unwrap_err();
 
     let expected_url = "file:///%E3%82%86%E3%82%8B%E3%82%86%E3%82%8A/77777777-7777-7777-7777-777777777777.png";
     assert_matches!(actual.kind(), ErrorKind::ObjectNotFound { url } if url == expected_url);
@@ -490,7 +490,7 @@ async fn entry_fails_with_invalid_filename() {
     let root_dir = tempdir().unwrap();
 
     let repository = FilesystemObjectsRepository::new(collator, root_dir.path()).await.unwrap();
-    let actual = repository.get(EntryUrl::from_path_str("file://", "/ゆるゆり/\x00.png")).await.unwrap_err();
+    let actual = repository.entry(EntryUrl::from_path_str("file://", "/ゆるゆり/\x00.png")).await.unwrap_err();
 
     let expected_url = "file:///%E3%82%86%E3%82%8B%E3%82%86%E3%82%8A/%00.png";
     assert_matches!(actual.kind(), ErrorKind::ObjectUrlInvalid { url } if url == expected_url);
