208cd7a5170ee64eaa2b46810e214ac9e37c2372
Chitoku <odango@chitoku.jp>
2026-01-08 15:45:57 +0900

    Respond with `304 Not Modified` or `416 Range Not Satisfiable` if precondition is not met when serving media

--- api/crates/application/src/service/objects.rs
+++ api/crates/application/src/service/objects.rs
@@ -3,8 +3,10 @@ use std::sync::Arc;
 use axum::{extract::{Query, State}, response::Response};
 use serde::Deserialize;
 
+use crate::Precondition;
+
 pub trait ObjectsServiceInterface: Send + Sync + 'static {
-    fn serve(&self, url: String) -> impl Future<Output = Response> + Send;
+    fn serve(&self, url: String, precondition: Option<Precondition>) -> impl Future<Output = Response> + Send;
 }
 
 #[derive(Deserialize)]
@@ -12,9 +14,9 @@ pub(crate) struct GetParams {
     url: String,
 }
 
-pub(crate) async fn serve<ObjectsService>(objects_service: State<Arc<ObjectsService>>, Query(GetParams { url }): Query<GetParams>) -> Response
+pub(crate) async fn serve<ObjectsService>(objects_service: State<Arc<ObjectsService>>, precondition: Option<Precondition>, Query(GetParams { url }): Query<GetParams>) -> Response
 where
     ObjectsService: ObjectsServiceInterface,
 {
-    objects_service.serve(url).await
+    objects_service.serve(url, precondition).await
 }
--- api/crates/application/src/tests/mocks/application/service/objects.rs
+++ api/crates/application/src/tests/mocks/application/service/objects.rs
@@ -1,11 +1,11 @@
 use axum::response::Response;
 
-use crate::service::objects::ObjectsServiceInterface;
+use crate::{Precondition, service::objects::ObjectsServiceInterface};
 
 mockall::mock! {
     pub(crate) ObjectsServiceInterface {}
 
     impl ObjectsServiceInterface for ObjectsServiceInterface {
-        fn serve(&self, url: String) -> impl Future<Output = Response> + Send;
+        fn serve(&self, url: String, precondition: Option<Precondition>) -> impl Future<Output = Response> + Send;
     }
 }
