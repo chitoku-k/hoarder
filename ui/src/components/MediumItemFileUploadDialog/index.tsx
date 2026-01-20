0c954a358c32006411a3b600b45187be96cd4986
Chitoku <odango@chitoku.jp>
2026-01-20 21:05:02 +0900

    Disable cancellation in the upload dialog after all the replicas are uploaded

--- ui/src/components/MediumItemFileUploadDialog/index.tsx
+++ ui/src/components/MediumItemFileUploadDialog/index.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import type { FunctionComponent } from 'react'
+import { useCallback, useRef } from 'react'
 import Dialog from '@mui/material/Dialog'
 
-import type { MediumItemFileUploadDialogBodyProps, UploadStatus } from '@/components/MediumItemFileUploadDialogBody'
+import type { MediumItemFileUploadDialogBodyProps, MediumItemFileUploadDialogBodyRef, UploadStatus } from '@/components/MediumItemFileUploadDialogBody'
 import MediumItemFileUploadDialogBody from '@/components/MediumItemFileUploadDialogBody'
 
 import styles from './styles.module.scss'
@@ -11,11 +12,18 @@ import styles from './styles.module.scss'
 const MediumItemFileUploadDialog: FunctionComponent<MediumItemFileUploadDialogProps> = ({
   close,
   ...props
-}) => (
-  <Dialog className={styles.dialog} open onClose={close}>
-    <MediumItemFileUploadDialogBody close={close} {...props} />
-  </Dialog>
-)
+}) => {
+  const ref = useRef<MediumItemFileUploadDialogBodyRef>(null)
+  const handleClose = useCallback(() => {
+    ref.current?.close()
+  }, [ ref ])
+
+  return (
+    <Dialog className={styles.dialog} open onClose={handleClose}>
+      <MediumItemFileUploadDialogBody ref={ref} close={close} {...props} />
+    </Dialog>
+  )
+}
 
 export type MediumItemFileUploadDialogProps = MediumItemFileUploadDialogBodyProps
 export type MediumItemFileUploadStatus = UploadStatus
