dbfbe43a06bd2ec16409af872332ebf4335856af
Chitoku <odango@chitoku.jp>
2026-09-26 17:56:38 +0900

    Remove `eslint-disable-next-line`s which are no longer necessary

--- ui/src/components/TagListColumnBodyEdit/index.tsx
+++ ui/src/components/TagListColumnBodyEdit/index.tsx
@@ -65,7 +65,6 @@ const TagListColumnBodyEdit: FunctionComponent<TagListColumnBodyEditProps> = ({
   }, [])
 
   const handleChangeAliases = useCallback((_e: SyntheticEvent, value: readonly string[]) => {
-    // eslint-disable-next-line @typescript-eslint/unbound-method
     const aliases = value.toSorted(collator.compare)
     setTag(tag => ({
       ...tag,
